e649fe9c4335f79d114639149f45f893abb50d85
Ruben-desktop <ruben-1337@hotmail.com>
2021-11-24 20:42:52 +0100

    # Changed and cleaned the code that checks the clicked side of the cube, which is now a part of Part. # Parts preview properly. # PartToBePlaced is deprecated. # UImanager changes selected part of the editor. # Placed parts are attached in eachothers list in the correct slot.
    TODO: implement the ubersick 3D array so you can attach parts to neighbours based on grid coordinates.

--- Toy World/Assets/Scripts/Part.cs
+++ Toy World/Assets/Scripts/Part.cs
@@ -28,8 +28,9 @@ public abstract class Part : MonoBehaviour
     /// </summary>
     /// <param name="partToAttachTo">Part that's seleted to connect to.</param>
     /// <param name="side">Side that's connecting.</param>
-    public void AttachPart(Part partToAttachTo, Orientation side)
+    public void AttachPart(Part partToAttachTo, Vector3 hitNormal)
     {
+        /*
         // check who we attach to
         if ((int)side % 2 == 0)
         {
@@ -43,6 +44,10 @@ public abstract class Part : MonoBehaviour
             attachedParts[(int)side - 1] = partToAttachTo;
             partToAttachTo.attachedParts[(int)side] = this;
         }
+        */
+        attachedParts[(int)DetermineSide(-hitNormal)] = partToAttachTo;
+        partToAttachTo.attachedParts[(int)partToAttachTo.DetermineSide(hitNormal)] = this;
+
         // check if any adjacent parts also exist and add them to my list
     }
 
@@ -55,4 +60,42 @@ public abstract class Part : MonoBehaviour
         Back,
         Front
     };
+
+    /// <summary>
+    /// Determines the side of the part that was clicked based on the hit normal. 
+    /// The normal is
+    /// </summary>
+    /// <param name="normal">Hit normal</param>
+    /// <returns></returns>
+    public Orientation DetermineSide(Vector3 normal)
+    {
+        Vector3 n = normal;
+        n = Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z) * n;
+        Debug.Log(n);
+
+        switch (n)
+        {
+            case Vector3 v when v.Equals(Vector3.up):
+                Debug.Log("Up");
+                return Orientation.Top;
+            case Vector3 v when v.Equals(Vector3.left):
+                Debug.Log("Left");
+                return Orientation.Left;
+            case Vector3 v when v.Equals(Vector3.back):
+                Debug.Log("Back");
+                return Orientation.Back;
+            case Vector3 v when v.Equals(Vector3.forward):
+                Debug.Log("Forward");
+                return Orientation.Front;
+            case Vector3 v when v.Equals(Vector3.down):
+                Debug.Log("Down");
+                return Orientation.Bottom;
+            case Vector3 v when v.Equals(Vector3.right):
+                Debug.Log("Right");
+                return Orientation.Right;
+            default:
+                Debug.Log("NORMAL IS INCORRECT - ERROR ERROR ERROR");
+                return Orientation.Right;//Not actually right at all
+        }
+    }
 }
--- Toy World/Assets/Scripts/Vehicle editor/VehicleEditor.cs
+++ Toy World/Assets/Scripts/Vehicle editor/VehicleEditor.cs
@@ -4,77 +4,121 @@ using UnityEngine;
 
 public class VehicleEditor : MonoBehaviour
 {
-    public static VehicleEditor instance;
+    private static VehicleEditor instance;
+    public static VehicleEditor _instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
 
-    [SerializeField] private TempPart coreBlock;
-    [SerializeField] private GameObject selectedPart;//
+    [SerializeField] private GameObject coreBlock;
+    [SerializeField] private GameObject selectedPart;
 
+    private GameObject previewedPart;
     private Vector3 prevMousePos;
+    private Quaternion partRotation;
 
     void Awake()
     {
-        if(instance == null)
-        {
-            instance = this;
-        }
-        else
-        {
-            Destroy(this);
-        }
+        //set the static instance
+        if (instance == null) { instance = this; }
+        else { Destroy(this); }
     }
 
     void Start()
     {
         SetSelectedPart(selectedPart);
-        if (coreBlock = null)
-        {
-            coreBlock = GameObject.Find("CoreBlock").GetComponent<TempPart>();
-        }
+        coreBlock = GameObject.Find("CoreBlock");
+
     }
 
     void Update()
     {
-        Func();
+        UpdateMouseInput();
     }
 
-    void Func()
+    void UpdateMouseInput()
     {
-        if (Input.mousePosition != prevMousePos)
+        RaycastHit hit = RaycastMousePosition();
+        if (hit.normal != Vector3.zero && hit.transform.TryGetComponent(out Part part)) //(hit.transform.GetComponent<TempPart>() != null)
         {
-            RaycastHit hit = RaycastMousePosition();
-            if (hit.transform.TryGetComponent(out TempPart part)) //(hit.transform.GetComponent<TempPart>() != null)
-            {
-                PreviewPart(hit.transform.position + hit.normal);//assuming box colliders
-            }
-            else if (selectedPart.activeSelf)
-            {
-                selectedPart.SetActive(false);
-            }
+            Debug.Log("part was hit");
+            PreviewPart(hit.transform.position + hit.normal);//assuming box colliders
+            Debug.Log($"hit pos: {hit.transform.position} hit normal: {hit.normal}");
+        }
+        else if (previewedPart.activeSelf)
+        {
+            previewedPart.SetActive(false);
+        }
 
-            prevMousePos = Input.mousePosition;
+        if (Input.GetMouseButtonDown(0))
+        {
+            PlaceSelectedPart(hit);
+            //NormalDirection(hit.normal);
         }
     }
 
-    void PlacePart(RaycastHit hit)
+    void NormalDirection(Vector3 normal)
     {
+        switch (normal)
+        {
+            case Vector3 v when v.Equals(Vector3.up):
+                Debug.Log("Up");
+                break;
+            case Vector3 v when v.Equals(Vector3.left):
+                Debug.Log("Left");
+                break;
+            case Vector3 v when v.Equals(Vector3.back):
+                Debug.Log("Back");
+                break;
+            case Vector3 v when v.Equals(Vector3.forward):
+                Debug.Log("Forward");
+                break;
+            case Vector3 v when v.Equals(Vector3.down):
+                Debug.Log("Down");
+                break;
+            case Vector3 v when v.Equals(Vector3.right):
+                Debug.Log("Right");
+                break;
+        }
 
+    }
+
+    void PlaceSelectedPart(RaycastHit hit)
+    {
+        GameObject placedPart = Instantiate(
+            selectedPart, hit.transform.localPosition + hit.normal + coreBlock.transform.position, partRotation,coreBlock.transform);
+        
+        if(placedPart.TryGetComponent(out Part part))
+        {
+            part.AttachPart(hit.transform.GetComponent<Part>(), hit.normal);
+        }
+
+        /* X instantiate selectedPart
+         * X call the attachPart on the raycast hit and the selected part, but with flipped normal(maybe automate this in the part class?)
+         * X figure out where to run the partToPlace code
+         * make a 3D array and check for neighbours to call the attach fuction for.
+         */
     }
 
     void PreviewPart(Vector3 pos)//todo:
     {
-        selectedPart.SetActive(true);
-        selectedPart.transform.position = pos;
+        previewedPart.SetActive(true);
+        previewedPart.transform.position = pos;
     }
 
-    public void SetSelectedPart(GameObject selectedPart)
+    public void SetSelectedPart(GameObject slctPart)
     {
-        Destroy(this.selectedPart);
-        this.selectedPart = selectedPart;
-        Instantiate(this.selectedPart);
-        if(selectedPart.TryGetComponent(out Collider col)){
+        Destroy(previewedPart);
+        selectedPart = slctPart;
+        previewedPart = Instantiate(selectedPart);
+        if (previewedPart.TryGetComponent(out Collider col))
+        {
             col.enabled = false;
         }
-        selectedPart.SetActive(false);
+        previewedPart.SetActive(false);
     }
 
     /* instantiate 3D array in which every part has it's coordinates 
@@ -95,9 +139,9 @@ public class VehicleEditor : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
-        Physics.Raycast(ray, out hit, 100);
-        Debug.Log("Hit location vector = "+hit.point);//remove debug later
+        Physics.Raycast(ray, out hit, 200);
+        //Debug.Log("Hit normal vector = "+hit.normal);//remove debug later
         return hit;
     }
-    
+
 }
