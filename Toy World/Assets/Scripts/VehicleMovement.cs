27f655113d35a00fbc32bf3065ea6d27ac914af5
KGroot <kwgroot@gmail.com>
2021-11-26 16:09:11 +0100

    Small cleanup of Debug's.

--- Toy World/Assets/Scripts/VehicleMovement.cs
+++ Toy World/Assets/Scripts/VehicleMovement.cs
@@ -67,7 +67,6 @@ public class VehicleMovement : MonoBehaviour
 
     public void MoveForward()
     {
-        Debug.Log(" Im moving ");
         transform.localPosition += transform.forward * movementSpeed;
         foreach (MovementPart movementPart in movementParts)
         {
