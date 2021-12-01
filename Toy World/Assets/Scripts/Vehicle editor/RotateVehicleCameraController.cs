d8c7f9cf3d70ab846e5b19c117e0bcee5f74b4e0
Léon Imanuel <leondino0309@gmail.com>
2021-12-01 14:46:20 +0100

    Added zoom function (also old input, but made new input easily integratable)

--- Toy World/Assets/Scripts/Vehicle editor/RotateVehicleCameraController.cs
+++ Toy World/Assets/Scripts/Vehicle editor/RotateVehicleCameraController.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class RotateVehicleCameraController : MonoBehaviour
 {
+    const int ZOOM_SPEED_DAMPNER = 10;
+
     // Speed factors for vehicle rotation and camera zooming
     [Range(1,3)]
     public float horizontalRotateSpeed, verticalRotateSpeed, cameraZoomSpeed;
@@ -21,13 +23,19 @@ public class RotateVehicleCameraController : MonoBehaviour
     // Temporary input in Update
     void Update()
     {
-        movementInput = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        float movementInputZ = 0;
+        if (Input.GetKey(KeyCode.Space))
+            movementInputZ = 1;
+        if (Input.GetKey(KeyCode.LeftShift))
+            movementInputZ = -1;
+
+        movementInput = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), movementInputZ);
     }
 
     private void FixedUpdate()
     {
         // Wordt in toekomst aangeroepn met Unity event en Input Actions
-        RotateVehicle(movementInput);
+        RotateVehicleMovement(movementInput);
     }
 
     /* Wordt in de toekomst something like:
@@ -35,12 +43,20 @@ public class RotateVehicleCameraController : MonoBehaviour
      * {
      *      Vector3 inputVector = context.ReadValue<Vector3>();
      *      vehicleCore.transform.Rotate(inputVector.y * verticalRotateSpeed, 
-            inputVector.x * -horizontalRotateSpeed, 0, Space.World);
+     *      inputVector.x * -horizontalRotateSpeed, 0, Space.World);
+     *      Zoom(inputVector.z);
      * }
      */
-    private void RotateVehicle(Vector3 inputVector)
+    private void RotateVehicleMovement(Vector3 inputVector)
     {
         vehicleCore.transform.Rotate(inputVector.y * verticalRotateSpeed, 
             inputVector.x * -horizontalRotateSpeed, 0, Space.World);
+        Zoom(inputVector.z);
+    }
+
+    private void Zoom(float zoomDirection)
+    {
+        zoomDirection /= ZOOM_SPEED_DAMPNER;
+        transform.position += transform.forward * zoomDirection * cameraZoomSpeed;
     }
 }
