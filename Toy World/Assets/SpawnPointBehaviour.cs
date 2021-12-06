4c22cde126b6f8de5d6883b1b6f9dc94059c860c
Thomas Haagsma <t_haagsma1998@hotmail.com>
2021-12-06 15:38:30 +0100

    added empty script to set spawn points locations

---
+++ Toy World/Assets/SpawnPointBehaviour.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPointBehaviour : MonoBehaviour
+{
+    void Start()
+    {
+        SetSpawnPointsLocations();
+    }
+
+    private void SetSpawnPointsLocations()
+    {
+        // Set spawn locations of the spawn points
+    }
+}
