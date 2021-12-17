6ed4dfbb0f4696057778e88db07a23aa22659973
Léon Imanuel <leondino0309@gmail.com>
2021-12-17 12:35:53 +0100

    Fixed the performance issue which was really just a coding error xD

--- Toy World/Assets/Scripts/Camera/ThirdPersonCameraControler.cs
+++ Toy World/Assets/Scripts/Camera/ThirdPersonCameraControler.cs
@@ -1,19 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ThirdPersonCameraControler : RotatingCameraController
 {
+    private const int UP_ROTATION_MAX = 85;
+    private const int DOWN_ROTATION_MAX = 275;
+    private const int ROTATION_SPEED_DAMPNER = 10;
+
+    private void Awake()
+    {
+        verticalRotateSpeed /= ROTATION_SPEED_DAMPNER;
+        horizontalRotateSpeed /= ROTATION_SPEED_DAMPNER;
+    }
+
     public override void RotateVehicleMovement(Vector3 inputVector)
     {
         base.RotateVehicleMovement(inputVector);
+        transform.RotateAround(vehicleCore.transform.position, Vector3.up, inputVector.x * horizontalRotateSpeed);
 
-        transform.RotateAround(vehicleCore.transform.position, Vector3.up, inputVector.x * (horizontalRotateSpeed/10));
-
-        if (transform.rotation.eulerAngles.x + -inputVector.y * verticalRotateSpeed < 89 
-            || transform.rotation.eulerAngles.x + -inputVector.y* verticalRotateSpeed > 271)
+        if (transform.rotation.eulerAngles.x + -inputVector.y * verticalRotateSpeed < UP_ROTATION_MAX
+            || transform.rotation.eulerAngles.x + -inputVector.y * verticalRotateSpeed > DOWN_ROTATION_MAX)
         {
-            transform.RotateAround(vehicleCore.transform.position, transform.right, -inputVector.y * (verticalRotateSpeed / 10));
+            transform.RotateAround(vehicleCore.transform.position, transform.right, -inputVector.y * verticalRotateSpeed);
         }
     }
 }
