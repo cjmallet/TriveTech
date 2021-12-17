e8fef5fe41fbac876e1651202edb20620bd76b5f
Thomas Haagsma <t_haagsma1998@hotmail.com>
2021-12-17 14:46:57 +0100

    Added EscMenu script for the esc menu behaviours

---
+++ Toy World/Assets/Scripts/UI Scripts/EscMenuBehaviour.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscMenuBehaviour : MonoBehaviour
+{
+    private Transform coreBlockStartTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Resume()
+    {
+
+    }
+
+    public void Restart()
+    {
+
+    }
+}
