025c77b64cd56cd55834009d1089962d216b09a8
Aslesh <cjullienmallet@gmail.com>
2021-12-21 10:30:00 +0100

    add save functionality to finishing levels and showing completion in level select UI

--- Toy World/Assets/Scripts/UI Scripts/MainMenu.cs
+++ Toy World/Assets/Scripts/UI Scripts/MainMenu.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
@@ -13,7 +12,9 @@ public class MainMenu : MonoBehaviour
     private GameObject contentHolder;
     private List<SceneAsset> levels= new List<SceneAsset>();
 
-    // Start is called before the first frame update
+    /// <summary>
+    /// Initialize the level select UI with correct completion
+    /// </summary>
     void Start()
     {
         contentHolder = GetComponentInChildren<GridLayoutGroup>().gameObject;
@@ -21,10 +22,16 @@ public class MainMenu : MonoBehaviour
 
         foreach (SceneAsset scene in levels)
         {
-            GameObject button= Instantiate(buttonPrefab,contentHolder.transform);
-            button.transform.localScale = new Vector3(1,1,1);
+            GameObject button = Instantiate(buttonPrefab, contentHolder.transform);
+            button.transform.localScale = new Vector3(1, 1, 1);
             button.name = scene.name;
-            button.GetComponent<Button>().onClick.AddListener(() => { button.GetComponent<GenericUIChoices>().LoadScene(scene.name);});
+            button.GetComponent<Button>().onClick.AddListener(() => { button.GetComponent<GenericUIChoices>().LoadScene(scene.name); });
+
+            //If the level has been completed
+            if (PlayerPrefs.GetInt(scene.name)==1)
+            {
+                button.GetComponent<Image>().color = new Color32(255,255,0,255);
+            }
         }
     }
 }
