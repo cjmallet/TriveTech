a3b6b0d397eb17d305b474a80e4999afbd5312a0
Aslesh <cjullienmallet@gmail.com>
2022-01-12 12:24:01 +0100

    minor code improvements and set limit for destructionspeed

--- Toy World/Assets/Scripts/EnemyScripts/ObstacleScripts/DestructibleObject.cs
+++ Toy World/Assets/Scripts/EnemyScripts/ObstacleScripts/DestructibleObject.cs
@@ -9,26 +9,13 @@ public class DestructibleObject : MonoBehaviour
     [SerializeField] private BoxCollider collideBox;
     private bool collided;
 
-    private float CalculateSpeed(float directionalSpeed)
-    {
-        if (directionalSpeed<0)
-        {
-            return -directionalSpeed;
-        }
-        else
-        {
-            return directionalSpeed;
-        }
-    }
-
     private void OnTriggerEnter(Collider collision)
     {
         if (!collided)
         {
             Rigidbody rb = collision.transform.GetComponentInParent<Rigidbody>();
-            float speed = CalculateSpeed(rb.velocity.x) + CalculateSpeed(rb.velocity.y) + CalculateSpeed(rb.velocity.z);
 
-            if (speed>=destructionSpeed)
+            if (rb.velocity.magnitude>=destructionSpeed)
             {
                 Instantiate(particles, transform.position, transform.rotation);
                 Destroy(transform.gameObject);
@@ -38,7 +25,6 @@ public class DestructibleObject : MonoBehaviour
             {
                 collideBox.enabled = true;
             }
-            Debug.Log(speed+" "+ rb.velocity.x+" "+rb.velocity.y+" "+rb.velocity.z);
         }
     }
 
