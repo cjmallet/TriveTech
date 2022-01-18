f9c7810377673707cc70e181cacc12e60a853d99
Aslesh <cjullienmallet@gmail.com>
2022-01-18 12:08:40 +0100

    fix level select screen

--- Toy World/Assets/Scripts/UI Scripts/MainMenu.cs
+++ Toy World/Assets/Scripts/UI Scripts/MainMenu.cs
@@ -1,8 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,29 +11,48 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private GameObject buttonPrefab;
 
     private GameObject contentHolder;
-    private List<SceneAsset> levels= new List<SceneAsset>();
+    private List<UnityEngine.Object> levels= new List<UnityEngine.Object>();
+    private List<int> levelNumbers = new List<int>();
+    private char[] charToTrim;
+    private string levelPrefix = "Level ";
 
     /// <summary>
     /// Initialize the level select UI with correct completion
     /// </summary>
     void Start()
     {
+        charToTrim = levelPrefix.ToArray();
         contentHolder = GetComponentInChildren<GridLayoutGroup>().gameObject;
-        levels = Resources.LoadAll("Levels", typeof(SceneAsset)).Cast<SceneAsset>().ToList();
+        levels = Resources.LoadAll("Levels").ToList();
 
-        foreach (SceneAsset scene in levels)
+        foreach (UnityEngine.Object level in levels)
         {
+            levelNumbers.Add(GetLevelNumber(level.name));
+        }
+
+        levelNumbers.Sort();
+
+        foreach (int levelNumber in levelNumbers)
+        {
+            string levelName = levelPrefix+levelNumber.ToString();
             GameObject button = Instantiate(buttonPrefab, contentHolder.transform);
             button.transform.localScale = new Vector3(1, 1, 1);
-            button.name = scene.name;
-            button.GetComponentInChildren<TextMeshProUGUI>().text = scene.name;
-            button.GetComponent<Button>().onClick.AddListener(() => { button.GetComponent<GenericUIChoices>().LoadScene(scene.name); });
+            button.name = levelName;
+            button.GetComponentInChildren<TextMeshProUGUI>().text = levelName;
+            button.GetComponent<Button>().onClick.AddListener(() => { button.GetComponent<GenericUIChoices>().LoadScene(levelName); });
 
             //If the level has been completed
-            if (PlayerPrefs.GetInt(scene.name)==1)
+            if (PlayerPrefs.GetInt(levelName) == 1)
             {
                 button.GetComponent<Image>().color = new Color32(255,255,0,255);
             }
         }
+    }
+
+    private int GetLevelNumber(string sceneName)
+    {
+        string levelString = sceneName.Trim(charToTrim);
+        int levelNumber = Int32.Parse(levelString);
+        return levelNumber;
     }
 }
