06bec52bb5812abea07cb39b0a61dfe784fa4ad1
Léon Imanuel <leondino0309@gmail.com>
2022-01-28 13:29:23 +0100

    Cargo Spawner now has basic line renderer laser to indicate where the cargo will drop

---
+++ Toy World/Assets/Scripts/Level scripts/LaserIndicator.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserIndicator : MonoBehaviour
+{
+    private LineRenderer lrLaser;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        lrLaser = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ShowLaser();
+    }
+
+    private void ShowLaser()
+    {
+        lrLaser.SetPosition(0, transform.position + transform.up);
+
+        RaycastHit laserHit;
+        if (Physics.Raycast(transform.position, -transform.up, out laserHit))
+        {
+            lrLaser.SetPosition(1, laserHit.point);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, transform.position - transform.up * 10);
+    }
+}
