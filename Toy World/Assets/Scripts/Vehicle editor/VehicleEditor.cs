3d79e78e46e76e5bcce84d0a479369b14d94e4fa
KGroot <kwgroot@gmail.com>
2022-02-02 14:32:59 +0100

    SUFFER.

--- Toy World/Assets/Scripts/Vehicle editor/VehicleEditor.cs
+++ Toy World/Assets/Scripts/Vehicle editor/VehicleEditor.cs
@@ -137,33 +137,35 @@ public class VehicleEditor : MonoBehaviour
         }
     }
 
-    public void RotatePart(InputAction.CallbackContext context)
+    public void RotatePartYaxis(InputAction.CallbackContext context)
     {
         if (context.performed && !previewedPart.name.Contains("Wheel") && !previewedPart.name.Contains("Launcher"))
         {
-            partRotation.eulerAngles = Vector3Int.RoundToInt(partRotation.eulerAngles + new Vector3(0, 90, 0));
+            previewedPart.transform.Rotate(0, 90, 0);
+            partRotation = previewedPart.transform.rotation; // You know what, fuck this.
+
             PlacePart(context);
         }
     }
 
-    public void RotatePartVertical(InputAction.CallbackContext context)
+    public void RotatePartXaxis(InputAction.CallbackContext context)
     {
         if (context.performed && !previewedPart.name.Contains("Wheel") && !previewedPart.name.Contains("Launcher"))
         {
-            if (vCount == 2)
-            {
-                partRotation.eulerAngles = Vector3Int.RoundToInt(partRotation.eulerAngles + new Vector3(-90, 0, 0));
-            }
-            else
-            {
-                partRotation.eulerAngles = Vector3Int.RoundToInt(partRotation.eulerAngles + new Vector3(90, 0, 0));
-            }
+            previewedPart.transform.Rotate(90, 0, 0);
+            partRotation = previewedPart.transform.rotation; // You know what, fuck this.
 
-            vCount++;
-            if (vCount == 4)
-            {
-                vCount = 0;
-            }
+            PlacePart(context);
+        }
+    }
+
+    public void RotatePartZaxis(InputAction.CallbackContext context)
+    {
+        if (context.performed && !previewedPart.name.Contains("Wheel") && !previewedPart.name.Contains("Launcher"))
+        {
+            previewedPart.transform.Rotate(0, 0, 90);
+            partRotation = previewedPart.transform.rotation; // You know what, fuck this.
+
             PlacePart(context);
         }
     }
